b90cefa14a7a57d3fed2199a4312ab72bbbd44e9
Junwei, Chen <junwei.chen@downtown8.com>
2020-01-23 14:30:20 -0500

    feat: delete DoublyLinkedList

--- src/data-structures/doubly-linked-list/DoublyLinkedList.js
+++ src/data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -8,7 +8,10 @@ export default class DoublyLinkedList {
   }
 
   toString(func) {
-    if (!this.head || !this.head.next) return ''
+    if (!this.head) return ''
+    if (!this.head.next) {
+      return this.head.value.toString()
+    }
 
     let currentNode = this.head
     const list = []
@@ -51,6 +54,60 @@ export default class DoublyLinkedList {
     const newNode = new DoublyLinkedListNode(value, currentNode, null)
     this.head = newNode
   }
-  
 
+  fromArray(array) {
+    if (array.length === 1) {
+      this.initList(array[0])
+      return
+    }
+    this.head = new DoublyLinkedListNode(array[0])
+
+    array.slice(1).forEach(value => this.append(value))
+  }
+
+  /**
+   * @returns {array}
+   */
+  toArray() {
+    if (!this.head) {
+      return []
+    }
+
+    const result = []
+    let currentNode = this.head
+    while (currentNode && currentNode.value) {
+      result.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+
+    return result
+  }
+
+  /**
+   * @param {any} value
+   * @description delete nodes by given value
+   */
+  delete(value) {
+    let deleteNode = null
+    if (!this.head) {
+      return deleteNode
+    }
+    const newNodeList = new DoublyLinkedList()
+    let currentNode = this.head
+    while (currentNode && currentNode.value) {
+      if (currentNode.value !== value) {
+        newNodeList.append(currentNode.value)
+      } else {
+        deleteNode = new DoublyLinkedListNode(currentNode.value)
+      }
+      currentNode = currentNode.next
+    }
+
+    if (deleteNode) {
+      this.head = newNodeList.head
+      this.tail = newNodeList.tail
+    }
+
+    return deleteNode
+  }
 }
